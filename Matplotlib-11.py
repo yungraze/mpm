9f56c586807f50976c4dbcbfc76151d6d904bdd9
AstroBFU <48380696+AstroBFU@users.noreply.github.com>
2020-09-03 22:15:43 +0200

    Rectangle

---
+++ Matplotlib-11.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 27 11:54:12 2020
+
+@author: Артур
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.patches as mpatches
+
+fig = plt.figure()
+
+plt.scatter(0.0, 0.0)
+mpatches.Polygon([[0,0],[1,0],[0,1]])
+
+plt.show()
